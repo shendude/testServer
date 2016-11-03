dcc0d960ad94e2cfea86b3d32ed0fcf2439e7aeb
shendude <shendude@hotmail.com>
2016-11-02 19:21:35 -0700

    image uploader

--- server/server.js
+++ server/server.js
@@ -1,9 +1,31 @@
 var express = require('express');
-var quote = require('./quote');
+var path = require('path');
+var vision = require('@google-cloud/vision');
+var upload = require('express-fileupload');
 
 
 var app = express();
-app.use('/quote', quote);
+
+app.use(upload());
+app.use('/', express.static(path.join(__dirname, '/../client')));
+
+app.post('/upload', function(req, res) {
+  var file;
+  if (!req.files) {
+    res.send('No picture uploaded');
+    return;
+  }
+  file = req.files.photo;
+  file.mv(path.join(__dirname, '/photos/file.jpg'), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send('file uploaded!');
+    }
+  });
+});
 
 
-app.listen(1337);
+app.listen(1337, function() {
+  console.log('listening to port 1337');
+});
